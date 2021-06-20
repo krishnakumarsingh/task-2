4781aef0412cd9812cce9d3a1a26dd65c9c1c4b0
Krishna Kumar <krishna.k.kumar@accenture.com>
2021-06-20 21:38:10 +0530

    first commit

---
+++ js/site/custom.js
@@ -0,0 +1,91 @@
+(function ($) {
+  $("body").hide();
+  $.getJSON("../../assets/json/header.json", function (data) {
+    const { logo, navBar, slider } = data?.header;
+    /* Logo */
+    var $logo = $("#logo");
+    $logo.append(
+      `<a class="navbar-caption text-white display-2" href="/index.html" ctrl=${
+        logo.text
+      }>${!logo.logoPath ? logo.text : `<img src=${logo.logoPath} />`}</a>`
+    );
+    /* Nav bar */
+    var $menu = $("#navbarSupportedContent ul");
+    for (var item in navBar) {
+      var _menu = "";
+      var _submenuNavBar = navBar[item];
+      if (_submenuNavBar.length > 0) {
+        var _submenu = "";
+
+        for (var i = 0; i < _submenuNavBar.length; i++) {
+          _submenu += `<a
+          class="text-white dropdown-item display-4"
+          aria-expanded="false"
+          href=${_submenuNavBar[i]["link"]}
+      >
+        ${_submenuNavBar[i]["type"]}
+        </a>`;
+        }
+        _menu = `<li class="nav-item dropdown">
+          <a class="nav-link link text-white dropdown-toggle display-4" href="/" aria-expanded="false" data-toggle="dropdown-submenu">
+            ${item}
+          </a>
+          <div class="dropdown-menu">${_submenu}</div>
+        </li>`;
+      } else if (!Array.isArray(_submenuNavBar)) {
+        _menu = `<li class='nav-item'>
+          <a class='nav-link link text-white display-4' href=${_submenuNavBar.link}>
+              ${item}
+          </a>
+      </li>`;
+      }
+      $menu.append(_menu);
+    }
+    /* Nav bar */
+    /* Slider */
+    var $slider = $("#custom-id-slider");
+    if (slider.length > 0) {
+      var _slider = "";
+      for (var item in slider) {
+        console.log(slider[item]);
+        _slider += `<div class="row ${slider[item].className}-data">
+                <div class="custom-white col-md-12 col-lg-12 block-title">
+                    <div class="line line-title-big"></div>
+                    <h1 class="custom-section-title align-left custom-semibold pb-3 custom-fonts-style display-1">${slider[item].sliderText}</h1>
+                    <div class="custom-section-btn align-left">
+                        <a class="btn btn-custom-outline display-7" href=${slider[item].button.href}>${slider[item].button.text}</a>
+                    </div>
+                </div>
+            </div>`;
+      }
+      console.log(_slider);
+    }
+    $slider.prepend(_slider);
+
+    /* Slider */
+    $("body").show();
+  }).fail(function () {
+    return "An error has occurred.";
+  });
+  setInterval(() => {
+    var hasClassSlider1 = $(".custom-id-slider").hasClass("slider-1");
+    var hasClassSlider2 = $(".custom-id-slider").hasClass("slider-2");
+    var hasClassSlider3 = $(".custom-id-slider").hasClass("slider-3");
+    if (hasClassSlider1) {
+      $(".custom-id-slider")
+        .removeClass("slider-1")
+        .addClass("slider-2")
+        .fadeIn(1000);
+    } else if (hasClassSlider2) {
+      $(".custom-id-slider")
+        .removeClass("slider-2")
+        .addClass("slider-3")
+        .fadeIn(1000);
+    } else if (hasClassSlider3) {
+      $(".custom-id-slider")
+        .removeClass("slider-3")
+        .addClass("slider-1")
+        .fadeIn(1000);
+    }
+  }, 5000);
+})(jQuery);
